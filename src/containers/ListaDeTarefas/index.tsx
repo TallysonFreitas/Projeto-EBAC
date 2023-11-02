a56d8187ba9249aa5ca2aa12677424bbecaf8af4
Tallyson Freitas <tallyson.eu@outlook.com>
2023-11-02 09:21:17 -0300

    commit aula 5

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -2,25 +2,41 @@ import Tarefa from '../../components/Tarefa'
 import { Container } from './style'
 
 const ListaDeTarefas = () => {
+  const tarefas = [
+    {
+      titulo: 'estudar JavaScript',
+      descricao: 'ver aula do curso',
+      prioridade: 'importante',
+      status: 'pendente'
+    },
+    {
+      titulo: 'estudar JavaScript',
+      descricao: 'ver aula do curso',
+      prioridade: 'importante',
+      status: 'pendente'
+    },
+    {
+      titulo: 'estudar JavaScript',
+      descricao: 'ver aula do curso',
+      prioridade: 'importante',
+      status: 'pendente'
+    }
+  ]
+
   return (
     <Container>
       <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
       <ul>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
-        <li>
-          <Tarefa />
-        </li>
+        {tarefas.map((each) => (
+          <li key={each.titulo}>
+            <Tarefa
+              descricao={each.descricao}
+              titulo={each.titulo}
+              status={each.status}
+              prioridade={each.prioridade}
+            />
+          </li>
+        ))}
       </ul>
     </Container>
   )
